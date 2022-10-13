977dea392c5a3d6d474ef4442628357a0131d444
JHorger <Joshua_Horger@reyrey.com>
2022-10-13 14:17:24 -0500

    added migration statuses to articles & comments, included status keys in controllers, added status columns to models

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,5 @@ class CommentsController < ApplicationController
 
     private
         def comment_params
-            params.require(:comment).permit(:commenter, :body)
-        end
+            params.require(:comment).permit(:commenter, :body, :status)
 end
