142218f5bd3ea7376b463fcb09cb1eb104fd0e0d
JHorger <Joshua_Horger@reyrey.com>
2022-10-21 11:40:45 -0500

    added Picture model, added display/upload areas to view files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_153003) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_161034) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,7 +57,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_153003) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "pictures", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_pictures_on_article_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "articles"
+  add_foreign_key "pictures", "articles"
 end
